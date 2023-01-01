2fa4149fd54ac2b65e8e2b94af0d53534e2bdd82
Jeff Sparkes <jsparkes@gmail.com>
2023-01-01 03:43:11 -0500

    Add first useful test.

--- test/CardSpec.purs
+++ test/CardSpec.purs
@@ -2,9 +2,34 @@ module Test.CardSpec where
 
 import Prelude
 
+import Cards (Card(..), getInnovationTxt, parseCards)
+import Data.Array (length, (!!))
+import Data.Maybe (Maybe(..))
+import Effect.Class (liftEffect)
+import Effect.Class.Console (log)
 import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+
 
 cardSpec :: Spec Unit
 cardSpec = do
+  describe "single" do
+    it "does nothing" do
+      liftEffect $ log "In card spec test"
+      pure unit
   describe "Cards" do
-    it "succeeds" $ pure unit
+    it "parses cards from innovation.txt" do
+      log "Getting file"
+      input <- getInnovationTxt
+      cards <- liftEffect $ parseCards input
+  -- case c of 
+  --   Nothing -> unit
+  --   Just cards ->
+      log $ show $ length cards
+      log $ show  cards
+      length cards `shouldEqual` 105
+      case cards !! 0 of
+        Nothing -> "x" `shouldEqual` "y"
+        Just (Card card) -> 
+          log card.title
+          -- card.title `shouldEqual` "Agriculture"
--- test/Main.purs
+++ test/Main.purs
@@ -2,13 +2,11 @@ module Test.Main where
 
 import Prelude
 
-import Cards (readJsonFile)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Effect.Class.Console (log)
 import Test.Spec.Discovery (discover)
 import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = discover "Test\\.*Spec" >>= runSpec [ consoleReporter ] >>> launchAff_
+main = discover "Test.*Spec" >>= runSpec [ consoleReporter ] # launchAff_
