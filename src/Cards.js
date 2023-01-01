3b7bceff73bf52411723f3189e29b8c5af327607
Jeff Sparkes <jsparkes@gmail.com>
2023-01-01 03:42:12 -0500

    Use ES6 syntax.

--- src/Cards.js
+++ src/Cards.js
@@ -1,12 +1,13 @@
 // 'use strict';
+'use esversion: 6';
 
-shuffleArray = function (array) {
+export function shuffleArray (array) {
   return function () {
     return array
       .map((value) => ({ value, sort: Math.random() }))
       .sort((a, b) => a.sort - b.sort)
       .map(({ value }) => value);
   };
-};
+}
 
-export { shuffleArray };
+// export { shuffleArray };
