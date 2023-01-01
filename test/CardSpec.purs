a42a58c1c2d494b5a8052ffcbfc58df65603c8d9
Jeff Sparkes <jsparkes@gmail.com>
2023-01-01 16:55:33 -0500

    Top level needs to be named spec.  Add CardColor test.

--- test/CardSpec.purs
+++ test/CardSpec.purs
@@ -2,7 +2,7 @@ module Test.CardSpec where
 
 import Prelude
 
-import Cards (Card(..), getInnovationTxt, parseCards)
+import Cards (Card(..), CardColor(..), cardColorParse, getInnovationTxt, parseCards)
 import Data.Array (length, (!!))
 import Data.Maybe (Maybe(..))
 import Effect.Class (liftEffect)
@@ -11,25 +11,18 @@ import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 
 
-cardSpec :: Spec Unit
-cardSpec = do
-  describe "single" do
-    it "does nothing" do
-      liftEffect $ log "In card spec test"
-      pure unit
+spec :: Spec Unit
+spec = do
+  describe "CardColor" do
+    it "round trip" do
+      (show Green # cardColorParse) `shouldEqual` Green
   describe "Cards" do
     it "parses cards from innovation.txt" do
       log "Getting file"
       input <- getInnovationTxt
       cards <- liftEffect $ parseCards input
-  -- case c of 
-  --   Nothing -> unit
-  --   Just cards ->
-      log $ show $ length cards
-      log $ show  cards
       length cards `shouldEqual` 105
       case cards !! 0 of
         Nothing -> "x" `shouldEqual` "y"
         Just (Card card) -> 
-          log card.title
-          -- card.title `shouldEqual` "Agriculture"
+          card.title `shouldEqual` "Agriculture"
