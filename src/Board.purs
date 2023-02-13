49355667c92f7de625761806dfd8f58accc009ec
Jeff Sparkes <jsparkes@gmail.com>
2023-02-13 05:27:42 -0500

    Start of board related functions.

---
+++ src/Board.purs
@@ -0,0 +1,42 @@
+module Board where
+
+import Prelude
+
+import Data.Argonaut (class DecodeJson, class EncodeJson)
+import Data.Argonaut.Decode.Generic (genericDecodeJson)
+import Data.Argonaut.Encode.Generic (genericEncodeJson)
+import Data.Array.NonEmpty (NonEmptyArray, updateAt, (!!))
+import Data.Generic.Rep (class Generic)
+import Data.Maybe (Maybe(..))
+import Data.Show.Generic (genericShow)
+import Player (Player(..))
+import Player as Player
+
+
+newtype Board = Board
+  { players :: NonEmptyArray Player
+  }
+
+derive instance genericPlayer :: Generic Board _
+
+instance showPlayer :: Show Board where
+  show = genericShow
+
+instance encodePlayer :: EncodeJson Board where
+  encodeJson = genericEncodeJson
+
+instance decodePlayer :: DecodeJson Board where
+  decodeJson = genericDecodeJson
+
+getPlayer :: Board -> Int -> Maybe Player
+-- Need to range chec khere
+getPlayer (Board board) pos = board.players !! pos
+
+updatePlayer :: Board -> Player -> Int -> Board
+updatePlayer (Board board) player pos = do
+  let p = updateAt pos player board.players
+  case p of 
+    Nothing -> Board board
+    Just players -> Board (board { players = players})
+
+
