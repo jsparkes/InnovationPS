7efa76c4dedb1065d9df1ed2ef5fe3a379972a06
Jeff Sparkes <jsparkes@gmail.com>
2023-02-17 05:19:27 -0500

    Split player an AI actions for Argiculture

--- src/CardFunctions.purs
+++ src/CardFunctions.purs
@@ -6,13 +6,12 @@ import Board (Board(..))
 import Board as Board
 import Cards (Card(..))
 import Cards as Cards
-import Control.Comonad.Store (pos)
 import Data.Maybe (Maybe(..))
 import Player (Player(..))
 import Player as Player
 
 promptForCard :: Int -> String -> Maybe Card
-promptForCard playerPos prompt =
+promptForCard _playerPos _prompt =
   Nothing
 
 agriculture :: Board -> Int -> Card -> Board
@@ -20,8 +19,16 @@ agriculture b@(Board board) pos (Card card) = do
   let p = Board.getPlayer b pos
   case p of
     Nothing -> Board board -- should panic here!
-    Just player ->
-      case promptForCard pos card.dogmaCondition1 of
+    Just player -> do
+      let
+        dis =
+          if pos == 0 then
+            promptForCard pos card.dogmaCondition1
+          else
+            -- If we implment tree evaluation, we can try all possible cards.
+            -- Simpler to just pick the highest.
+            Player.getHighestCard player
+      case dis of
         Nothing -> Board board
         Just discard -> do
           let
